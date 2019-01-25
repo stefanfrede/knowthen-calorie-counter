83f8fd4e7af5805d52c7bee548f5c24caaa236c8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-25 15:19:19 +0000

    Deploy stefanfrede/knowthen-calorie-counter to github.com/stefanfrede/knowthen-calorie-counter.git:gh-pages

--- vendors~main.8e6f.js
+++ vendors~main.8e6f.js
@@ -1,4 +1,4 @@
-/*! v1.0.0-7-g93e52ad */
+/*! 93e52ad */
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],[function(t,n){var e=t.exports={version:"2.5.7"};"number"==typeof __e&&(__e=e)},function(t,n){t.exports=function(t){return"object"==typeof t?null!==t:"function"==typeof t}},function(t,n,e){t.exports=!e(3)(function(){return 7!=Object.defineProperty({},"a",{get:function(){return 7}}).a})},function(t,n){t.exports=function(t){try{return!!t()}catch(t){return!0}}},function(t,n,e){"use strict";
 /**
  * @license
