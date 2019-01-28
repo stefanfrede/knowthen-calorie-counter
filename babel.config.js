bcd6bcd4f3df5afc79d8e7d90d9c6c7ac09be6a2
Stefan Frede <git@frede.io>
2019-01-28 13:28:35 +0100

    Use jest env in babel config

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,19 @@
-const isTest = String(process.env.NODE_ENV) === 'test';
+module.exports = api => {
+  const isTest = api.env('test');
 
-module.exports = {
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        corejs: 2,
-      },
-    ],
-    isTest ? 'babel-plugin-dynamic-import-node' : null,
-  ].filter(Boolean),
-  presets: [['@babel/preset-env', { modules: isTest ? 'commonjs' : false }]],
+  return {
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
+      [
+        '@babel/plugin-transform-runtime',
+        {
+          corejs: 2,
+        },
+      ],
+      isTest ? 'babel-plugin-dynamic-import-node' : null,
+    ].filter(Boolean),
+    presets: [['@babel/preset-env', { modules: isTest ? 'commonjs' : false }]],
+  };
 };
